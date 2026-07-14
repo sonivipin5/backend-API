35bbb1fcdda2eb166584426f4e54f8b1e3a6f4b9
vipinsoni-sudo <vipin.soni@jcchaudhry.com>
2026-07-14 17:08:26 +0530

    feat: add morgan for logging and create logger middleware section 3, video 6

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const bootcamps = require('./routes/bootcamps.js');
+const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routes
 app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
