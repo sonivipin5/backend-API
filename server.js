e1be4386edc3e07268951d10f49628f331c78e73
vipinsoni-sudo <vipin.soni@jcchaudhry.com>
2026-07-15 14:25:45 +0530

    feat: add mongoose for MongoDB connection and implement database connection in server section 4, video 2

--- server.js
+++ server.js
@@ -4,9 +4,13 @@ const dotenv = require('dotenv');
 const bootcamps = require('./routes/bootcamps.js');
 const logger = require('./middleware/logger');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -22,4 +26,10 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });
